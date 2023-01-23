c5578260249531ca6207cba58ac4ec3aa261e365
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-23 19:17:24 +0100

    fix

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ tsk 1 advanced """
-from typing import List, Union, Any
+from typing import Sequence, Union, Any
 
 
-def safe_first_element(lst: List[Union[Any, None]]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Return the sum of a list of floats"""
     if lst:
         return lst[0]
