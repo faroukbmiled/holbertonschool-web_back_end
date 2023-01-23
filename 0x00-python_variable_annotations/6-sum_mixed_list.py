1ec3f20dc5f3324abe9407f8fa5bb74f40119927
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-23 18:39:46 +0100

    done batch 2

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" tsk 6"""
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return the sum of a list of floats"""
+    return sum(mxd_lst)
