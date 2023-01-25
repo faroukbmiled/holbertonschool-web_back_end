e8fdc0a2e37816bbf695ccaed48d4bc92eae661e
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-25 18:42:25 +0100

    ??

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,4 +7,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """async_comprehension"""
     async_gen = async_generator()
-    return [num async for num in async_gen if num < 10]
+    return [num async for num in async_gen]
