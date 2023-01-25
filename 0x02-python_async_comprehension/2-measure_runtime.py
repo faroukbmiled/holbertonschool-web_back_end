0834bad9b8125af387d9e326dbb976af4c01197b
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-25 20:00:26 +0100

    fix?

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,15 +2,13 @@
 """ async """
 import asyncio
 import time
-from typing import Tuple
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> Tuple[float, float]:
+async def measure_runtime() -> float:
     """ async """
     start_time = time.perf_counter()
-    await asyncio.gather(async_comprehension(), async_comprehension(),
-                         async_comprehension(), async_comprehension())
+    await asyncio.gather(async_comprehension())
     end_time = time.perf_counter()
     total_time = end_time - start_time
     return total_time
