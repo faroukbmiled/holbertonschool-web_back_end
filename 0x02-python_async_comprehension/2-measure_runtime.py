a31da42d7eb1ad20e545d0c085f873c09b7c0351
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-25 19:56:44 +0100

    final

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" async """
+import asyncio
+import time
+from typing import Tuple
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> Tuple[float, float]:
+    """ async """
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension())
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time
