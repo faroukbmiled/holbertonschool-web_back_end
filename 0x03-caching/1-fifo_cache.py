46240855dce00bf1432c9f58cb9e21cfbe33ff51
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-27 23:29:32 +0100

    up

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -12,19 +12,21 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item in the cache"""
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
 
-        if not key or not item:
-            return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1\
-                and (key not in self.cache_data.keys()):
-            print('DISCARD: {}'.format(self.keys[0]))
-            del self.cache_data[self.keys[0]]
-            self.keys = self.keys[1:]
-        self.cache_data[key] = item
-        self.keys.append(key)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                item_discarded = self.keys.pop(0)
+                del self.cache_data[item_discarded]
+                print("DISCARD:", item_discarded)
+
+            self.cache_data[key] = item
+            self.keys.append(key)
 
     def get(self, key):
         """Get an item by key"""
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data[key]
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
