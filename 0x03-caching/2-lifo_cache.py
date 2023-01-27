2da9877d70679022eaa5bb216aa786311dfa65bb
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-27 23:18:23 +0100

    tsk 2

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Basic cache"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """FIFOCache"""
+
+    def __init__(self):
+        """init"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return None
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = list(self.cache_data.keys())[-1]
+            print("DISCARD: {}".format(last_key))
+            del self.cache_data[last_key]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
