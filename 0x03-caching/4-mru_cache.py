e7e4ba688079c0258870d07b865ddfb39607e7d0
faroukbmiled <fcfaroukfc@gmail.com>
2023-01-27 23:58:19 +0100

    fix?

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -5,31 +5,30 @@ from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
     """
-    MRUCache class that inherits from BaseCaching and is a caching system
+    BasicCache class that inherits from BaseCaching and is a caching system
     """
     def __init__(self):
         """init"""
         super().__init__()
-        self.recent_keys = {}
+        self.recent_keys = []
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key is None or item is None:
-            raise ValueError("Key and item cannot be None")
+            return None
         if key in self.recent_keys:
-            self.recent_keys.pop(key)
+            self.recent_keys.remove(key)
         elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            mru_key = next(iter(self.recent_keys))
+            mru_key = self.recent_keys.pop()
             print("DISCARD: {}".format(mru_key))
-            self.recent_keys.pop(mru_key)
-            self.cache_data.pop(mru_key)
+            del self.cache_data[mru_key]
         self.cache_data[key] = item
-        self.recent_keys[key] = 0
+        self.recent_keys.insert(0, key)
 
     def get(self, key):
         """Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
-        self.recent_keys.pop(key)
-        self.recent_keys[key] = 0
+        self.recent_keys.remove(key)
+        self.recent_keys.insert(0, key)
         return self.cache_data[key]
