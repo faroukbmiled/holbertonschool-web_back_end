8e84946e4acca42a02cd214434fd7fcbec086a4d
faroukbmiled <fcfaroukfc@gmail.com>
2023-02-02 19:06:59 +0100

    init

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 import re
 from typing import List
 """filter_datum"""
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """filter_datum"""
     for field in fields:
         message = re.sub(fr'{field}=.+?{separator}',
