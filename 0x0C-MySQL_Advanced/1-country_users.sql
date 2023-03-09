f643570bf644bfb25157faf3b174740fd0f04ab6
faroukbmiled <fcfaroukfc@gmail.com>
2023-03-09 19:28:56 +0100

    init

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+--creates table users
+CREATE TABLE IF NOT EXISTS `users` (
+`id` INT NOT NULL UNIQUE AUTO_INCREMENT,
+`email` VARCHAR(255) NOT NULL UNIQUE,
+`name` VARCHAR(255),
+`country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
