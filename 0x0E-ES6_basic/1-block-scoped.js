551577e7f93b527dcecc3735b1cd7a0351f98182
faroukbmiled <fcfaroukfc@gmail.com>
2023-03-16 02:01:13 +0100

    fixed

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const newTask = true;
+    const newTask2 = false;
+    return [newTask, newTask2];
   }
 
   return [task, task2];
