362810edfd7c68f13d5d6d0e6737eb3e477598bf
faroukbmiled <fcfaroukfc@gmail.com>
2023-03-28 21:32:04 +0100

    get ids

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,15 @@ function updateStudentGradeByCity(getListStudents, city, newGrades) {
   return getListStudents
     .filter((student) => student.location === city)
     .map((student) => {
-      const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
+      const foundGrade = newGrades.filter(
+        (newGrade) => newGrade.studentId === student.id,
+      );
+
+      const grade = foundGrade[0] ? foundGrade[0].grade : 'N/A';
+
       return {
         ...student,
-        grade: foundGrade ? foundGrade.grade : "N/A",
+        grade,
       };
     });
 }
