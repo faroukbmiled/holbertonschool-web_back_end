0f00d673a139baeb02806dceceb76104a4139847
faroukbmiled <fcfaroukfc@gmail.com>
2023-03-28 23:18:40 +0100

    adv

---
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count++;
+  weakMap.set(endpoint, count);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { weakMap, queryAPI };
