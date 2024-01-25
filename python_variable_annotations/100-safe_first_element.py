96c45a6baf57e04dde8e3fd20d252c4d899b211f
faroukbmiled <fcfaroukfc@gmail.com>
2024-01-25 19:48:39 +0100

    add

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ tsk 1 advanced """
-from typing import Sequence, Union, Any
+from typing import Sequence, Optional, Any
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """Return the sum of a list of floats"""
     if lst:
         return lst[0]
     else:
         return None
-
