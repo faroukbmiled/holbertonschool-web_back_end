803aeec7b2d3acfe53f90be0aeace3c5991b01b7
faroukbmiled <fcfaroukfc@gmail.com>
2024-01-25 19:56:19 +0100

    add

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ tsk 1 advanced """
 
+from typing import Sequence, Any, Union
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """annotatiosafe_first_elementn"""
+    """annotationsafe_first_element"""
     if lst:
         return lst[0]
     else:
